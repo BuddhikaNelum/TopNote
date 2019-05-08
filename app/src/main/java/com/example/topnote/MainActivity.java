f00c7d97c831c2c685acae403c741f445088a50e
BuddhikaNelum <dlbnprabath@gmail.com>
2019-05-08 12:40:27 +0530

    login & register layout designed

--- app/src/main/java/com/example/topnote/MainActivity.java
+++ app/src/main/java/com/example/topnote/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.topnote;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLogin;
+    private Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.login);
+        btnSignUp = findViewById(R.id.signup);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signup();
+            }
+        });
+    }
+
+    public void login() {
+        AlertDialog.Builder mydialog = new AlertDialog.Builder(this);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View myview = inflater.inflate(R.layout.reg_log_input_layout, null);
+        mydialog.setView(myview);
+
+        AlertDialog dialog = mydialog.create();
+
+        dialog.show();
+    }
+
+    public void signup() {
+        AlertDialog.Builder mydialog = new AlertDialog.Builder(this);
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View myview = inflater.inflate(R.layout.signup_layout, null);
+        mydialog.setView(myview);
+
+        AlertDialog dialog = mydialog.create();
+
+        dialog.show();
     }
 }
