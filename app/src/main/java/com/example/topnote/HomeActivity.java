5feeef054fc13933881029215dcfd357fcde7522
BuddhikaNelum <dlbnprabath@gmail.com>
2019-05-08 15:13:51 +0530

    designed home layout

--- app/src/main/java/com/example/topnote/HomeActivity.java
+++ app/src/main/java/com/example/topnote/HomeActivity.java
@@ -1,13 +1,37 @@
 package com.example.topnote;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab_btn;
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        fab_btn = findViewById(R.id.fab);
+
+        recyclerView = findViewById(R.id.recycler);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setStackFromEnd(true);
+        layoutManager.setReverseLayout(true);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+        fab_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
